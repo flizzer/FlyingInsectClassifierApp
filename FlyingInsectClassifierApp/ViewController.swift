0d43a435abb69c19a855af3c2ddfde0bf06f00b5
flizzer <briandavis1977@gmail.com>
2019-07-16 18:17:22 -0400

    initial commit

---
+++ FlyingInsectClassifierApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlyingInsectClassifierApp
+//
+//  Created by Brian Davis on 7/16/19.
+//  Copyright © 2019 Brian Davis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
